039896264574ac2a4799c504c658f5f26a58fa10
athaax <jonathansolis@dhcp80ff3171.dynamic.uiowa.edu>
2013-09-30 13:24:15 -0500

    initial commit

---
+++ mysite/_config.php
@@ -0,0 +1,18 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'localhost',
+	"username" => 'root',
+	"password" => 'omega',
+	"database" => 'jonathan',
+	"path" => '',
+);
+
+// Set the site locale
+i18n::set_locale('en_US');
+Director::set_environment_type("dev");
